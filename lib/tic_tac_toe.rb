5632b0ef42eb0baead742ce99599e02e53ba78cd
Lauren Dove <elledoveinc@gmail.com>
2019-10-04 00:22:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,7 @@
 class TicTacToe 
   
   def intialize(board)
-    @board = board || array.new(9 , " ") 
+    @board = board || array.new(9 , " ") #We created a new array with 9 " " empty spaces
     
   end
 end
