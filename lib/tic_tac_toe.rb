b6b48717add855c63d47f3e133fcec952b7bddf6
Lauren Dove <elledoveinc@gmail.com>
2019-10-04 00:29:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,8 @@ require 'pry'
 class TicTacToe 
   
   def intialize(board)
-    @board = Array.new(9, " ") #We created a new array with 9 " " empty spaces
+    @board = board 
+    board = Array.new(9, " ") #We created a new array with 9 " " empty spaces
     binding.pry
     end
 end
